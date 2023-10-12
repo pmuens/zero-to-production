e6f930c67ae00a91dabb177acf907a7ae6312c39
Philipp Muens <philipp@muens.io>
2023-10-12 17:26:24 +0200

    Add support for configuration file

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use std::net::TcpListener;
-use zero_to_production::run;
+use zero_to_production::{configuration::get_configuration, startup::run};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let address = TcpListener::bind("127.0.0.1:8000")?;
-    run(address)?.await
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+    run(listener)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,4 +1,5 @@
 use std::net::TcpListener;
+use zero_to_production::startup::run;
 
 #[tokio::test]
 async fn health_check_works() {
@@ -64,7 +65,7 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port.");
     let port = listener.local_addr().unwrap().port();
-    let server = zero_to_production::run(listener).expect("Failed to bind address.");
+    let server = run(listener).expect("Failed to bind address.");
     tokio::spawn(server);
     format!("http://127.0.0.1:{}", port)
 }
